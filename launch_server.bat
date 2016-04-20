0a92dd30012db1d1a85750091641473f6ca6c1b3
Joseph Hancock <hancockjr@gmail.com>
2016-04-19 21:21:52 -0600

    SCRIPT: fix path in server script

--- launch_server.bat
+++ launch_server.bat
@@ -1,3 +1,3 @@
 @echo off
-C:\cygwin64\bin\bash.exe --login -i -c 'cd "/cygdrive/c"; pushd Users/joseph/Downloads/mobile_app_dev/pmic/; python -m SimpleHTTPServer; popd'
+C:\cygwin64\bin\bash.exe --login -i -c 'cd "/cygdrive/c"; pushd Users/joseph/Documents/git/pmic/; python -m SimpleHTTPServer; popd'
 
